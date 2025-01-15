e93326954331b32b74731adab95f1c6e6eb4aa6d
Ellen Kramp <ellenkramp@gmail.com>
2025-01-14 19:07:10 -0500

    update resume and certs

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,4 +1,5 @@
 import { Caprasimo } from "next/font/google";
+import Script from "next/script";
 import "./styles.css";
 import resume from "../../resume.json";
 
@@ -11,18 +12,32 @@ const ResumePage = () => {
   const download = () => {
     const link = document.createElement("a");
     link.href = "/EllenKramp-SoftwareEngineer_FullResume_2024.pdf";
-    link.setAttribute(
-      "download",
-      "EllenKramp-SoftwareEngineer_FullResume_2024.pdf"
-    );
+    link.setAttribute("download", "EllenKramp_FullStack_Resume_2025.pdf");
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
   return (
-    <div className="section flex flex-col items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
+    <div className="section flex flex-col items-center justify-items-center p-8 pb-20 sm:p-10">
+      <h1 className={`text-2xl sm:text-4xl ${caprasimo.className}`}>Summary</h1>
       <div>
-        <p className="md:text-3xl text-xl">{resume.summary}</p>
+        <p className="sm:text-3xl text-xl">{resume.summary}</p>
+      </div>
+      <h1 className={`text-2xl sm:text-4xl ${caprasimo.className}`}>
+        Certifications
+      </h1>
+      <div className="iframe-container">
+        <iframe
+          data-iframe-width="150"
+          data-iframe-height="270"
+          data-share-badge-id="d71dc6b3-e463-4b0a-ac6b-d9678468f474"
+          data-share-badge-host="https://www.credly.com"
+        ></iframe>
+        <Script
+          type="text/javascript"
+          async
+          src="//cdn.credly.com/assets/utilities/embed.js"
+        />
       </div>
       <h1 className={`text-2xl md:text-4xl ${caprasimo.className}`}>
         Previous Roles
